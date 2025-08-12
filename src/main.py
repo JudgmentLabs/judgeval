990d96945f62c2fef1b06ca0f48b10b03a19d1c5
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2025-08-12 13:32:52 -0700

    chore: dataset fix

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import os
 
 JUDGMENT_ORG_ID = "27c7d380-6cb0-495f-b27c-8c1ff82b72af"
 JUDGMENT_API_KEY = "736046e3-b3a2-4a1a-bd3e-af6a6f8a9a5d"
-JUDGMENT_API_URL = "http://localhost:8000"
-
+# JUDGMENT_API_URL = "http://localhost:8000"
+JUDGMENT_API_URL = "https://staging.api.judgmentlabs.ai"
 
 os.environ["JUDGMENT_API_KEY"] = JUDGMENT_API_KEY
 os.environ["JUDGMENT_ORG_ID"] = JUDGMENT_ORG_ID
@@ -24,20 +24,19 @@ tracer = Tracer(
 
 client = JudgmentClient()
 
-examples = [
-    Example(
-        input="What is the capital of France?",
-        actual_output="Paris is the capital of France.",
-        expected_output="Paris",
-    )
-]
+from judgeval.dataset import Dataset
 
 
-from judgeval.scorers import AnswerRelevancyScorer
+from judgeval.dataset import Dataset
+from judgeval.data import Example
 
-results = client.run_evaluation(
-    project_name="ahh",
-    eval_run_name="test",
-    examples=examples,
-    scorers=[AnswerRelevancyScorer(threshold=0.9)],
-)
+# dataset = Dataset.create(
+#     name="qa_dataset",
+#     project_name="default_project",
+#     examples=[Example(input="...", actual_output="...")],
+# )
+
+dataset = Dataset.get(name="qa_dataset", project_name="default_project")
+
+print(dataset)
+
