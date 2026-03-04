77937487db47c07bbf0bf17cd0e3611b10642103
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2026-03-03 18:07:16 -0800

    test: remove scorers

--- src/judgeval/v1/scorers/base_custom_scorer/trace_scorer.py
+++
@@ -1,5 +0,0 @@
-from judgeval.v1.scorers.base_custom_scorer.base_custom_scorer import BaseCustomScorer
-from judgeval.v1.internal.api.models import TraceSpan
-from typing import List
-
-TraceScorer = BaseCustomScorer[List[TraceSpan]]
